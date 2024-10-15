ca91ee633050f2672d571afcb7dbf7e8951c6adf
Hawk5778 <takayan.fps@gmail.com>
2024-10-15 12:15:36 +0900

    gui fix

--- Bingo.py
+++ Bingo.py
@@ -1,48 +1,58 @@
-#pip install flet 
-
 import random
 from random import randint
-import tkinter
+import tkinter as tk
+import csv
 
-# y = []
-# a = 0
+y = []
 
-# x = randint(1, 75)
-# z = x in y
-# y.append(x)
-
-# print(x)
-# print(y)
-# print(z)
-
-def bingo():
+def bingo(event):
+    global x,y,z,b
+    a = 0
     while a == 0:
-        y.append(x)
         x = randint(1, 75)
         z = x in y
+        b = len(y)
+        print("結果", b)
         if z == True:
-            continue
+            if b == 75:
+                label2["text"] = "数字がなくなりました。"
+                break
+            else:
+                continue
         else:
             print(x)
-            if x == 75:
-                print(y)
-                break
+            label2["text"] = x
+            y.append(x)
+            break
+
+
+def rireki(event):
+    label4["text"] = y
 
 #GUI
-root = tkinter.Tk()
+root = tk.Tk()
 
-root.title(u"Software Title") #title
+root.title(u"BINGO") #title
 root.geometry("400x300") #size
 
-Static1 = tkinter.Label(text = u"test") #label 色指定(, foreground='#ff0000', background='#ffaacc')
-#Static1.place(x=150, y=228)#place
-Static1.pack()
+label1 = tk.Label(text=u"ビンゴ",font=("Arial", 25))
+label2 = tk.Label(text=u"表示先",font=("Times",16)) 
+label3 = tk.Label(text=u"履歴",font=("Arial", 20))
+label4 = tk.Label(text=u"表示先",font=("Times",16))
+label1.pack()
+label2.pack()
+label3.pack()
+label4.pack()
 
-Button1 = tkinter.Button(text=u"Go!", width=10)
-Button1.bind("<Button-1>", bingo)
-Button2 = tkinter.Button(text=u"履歴", width=10)
+Button1 = tk.Button(text=u"Go!", width=10)
+Button2 = tk.Button(text=u"履歴", width=10)
+Button3 = tk.Button(text=u"csv出力",width=10)
 Button1.pack()
 Button2.pack()
+Button3.pack()
+
+Button1.bind("<Button-1>", bingo)
+Button2.bind("<Button-1>", rireki)
 
 root.mainloop()
 
@@ -50,3 +60,4 @@ root.mainloop()
 
 
 #https://qiita.com/nnahito/items/ad1428a30738b3d93762
+#https://pythonsoba.tech/tkinter/
